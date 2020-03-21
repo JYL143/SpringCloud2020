51246f5dd2d275dad18fd7de92838a29ac588161
jyl <1432364557@qq.com>
2020-03-21 17:10:06 +0800

    分布式配置中心 3-21 17:10

---
+++ cloud-config-client-3355/src/main/java/com/jyl/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.jyl.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RefreshScope   //监控依赖，yml暴露端口，做动态刷新，还需要发post请求才可以动态刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
